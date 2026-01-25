a8639f208990020d54a20c99513bcc9fd5c00871
molesir <1063441425@qq.com>
2026-01-25 22:45:52 +0800

    container

--- lumen-python/lumen/nn/modules/container.py
+++ lumen-python/lumen/nn/modules/container.py
@@ -1,5 +1,6 @@
 from . import Module
-from typing import Optional, Iterable
+from .. import Parameter
+from typing import Optional, Iterable, Mapping, Tuple
 
 
 class ModuleList(Module):
@@ -20,4 +21,57 @@ class ModuleList(Module):
         
     def __len__(self) -> int:
         return len(self._modules)
+    
+
+class ModuleDict(Module):
+    """Holds submodules in a dictionary."""
+    def __init__(self, modules: Optional[Mapping[str, Module]] = None) -> None:
+        super().__init__()
+        if modules is not None:
+            self.update(modules)
+
+    def __getitem__(self, key: str) -> Module:
+        return self._modules[key]
+
+    def __setitem__(self, key: str, module: Module) -> None:
+        self.register_module(key, module)
+
+    def __len__(self) -> int:
+        return len(self._modules)
+    
+    def __contains__(self, key: str) -> bool:
+        return key in self._modules
+
+    def update(self, modules: Mapping[str, Module]) -> None:
+        for n, m in enumerate(modules):
+            self.register_module(n, m)
+            
+    def keys(self) -> Iterable[str]:
+        return self._modules.keys()
+    
+    def items(self) -> Iterable[Tuple[str, Module]]:
+        return self._modules.items()
+    
+    def values(self) -> Iterable[Module]:
+        return self._modules.values()
+
+
+class ParameterList(Module):
+    """Holds parameters in a list."""
+
+    def __init__(self, params: Optional[Iterable[Parameter]] = None) -> None:
+        super().__init__()
+        if params is not None:
+            self.extend(params)
+
+    def extend(self, params: Iterable[Parameter]):
+        offset = len(self)
+        for i, param in enumerate(params):
+            self.register_parameter(str(offset + i), param)
+        
+    def __getitem__(self, idx: int) -> Parameter:
+        return self._parameters[str(idx)]
+        
+    def __len__(self) -> int:
+        return len(self._parameters)
     
